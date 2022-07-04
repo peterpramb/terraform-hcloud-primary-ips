38b93682e0ca5fc639440a6a6783cf6dd03b54c1
Peter Pramberger <peterpramb@member.fsf.org>
2022-07-04 14:33:26 +0200

    Fix output variables

--- outputs.tf
+++ outputs.tf
@@ -26,13 +26,13 @@ output "primary_ips" {
 output "primary_ip_ids" {
   description = "A map of all primary IP objects indexed by ID."
   value       = {
-    for primary_ip in local.primary_ips : primary_ip.id => primary_ip
+    for primary_ip in local.output_primary_ips : primary_ip.id => primary_ip
   }
 }
 
 output "primary_ip_names" {
   description = "A map of all primary IP objects indexed by name."
   value       = {
-    for primary_ip in local.primary_ips : primary_ip.name => primary_ip
+    for primary_ip in local.output_primary_ips : primary_ip.name => primary_ip
   }
 }
